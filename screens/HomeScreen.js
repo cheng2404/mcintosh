75f1d96beded0c583f23187fd92e3a54f6d034da
Taiki <taikikoyama0701@gmail.com>
2019-08-25 12:23:07 +1000

    Move codes from github

---
+++ screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+export default class HomeScreen extends Component {
+
+  // To hide the NavigationBar from Home Screen
+  static navigationOptions = {
+    header: null
+  };
+
+  // TODO: Add contents on the HOME Screen.
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    )
+  }
+}
+
+
+// TODO: Add styles on the HOME Screen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+})
