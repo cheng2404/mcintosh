f18d7fc461e1a13374ee930887966d9fd7f8097a
Taiki <taikikoyama0701@gmail.com>
2019-08-26 15:53:20 +1000

    add containers on the dashboard screen

--- components/Dashboard/Header.js
+++ components/Dashboard/Header.js
@@ -4,13 +4,31 @@ import {
   View,
   StyleSheet
 } from 'react-native';
+import { vw } from 'react-native-expo-viewport-units';
+
 
 export default class Header extends Component {
   render() {
     return (
-      <View>
-        <Text>Header</Text>
+      <View style={styles.header}>
+        <Text style={styles.text}>Header</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: vw(100),
+    height: 70,
+    marginTop: 50,
+    backgroundColor: '#2f225b',
+  },
+
+  text: {
+    color: '#ffffff'
+  }
+})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import {
 } from 'react-native';
 
 import Header from '../components/Dashboard/Header'
+import { vw } from 'react-native-expo-viewport-units';
 
 export default class HomeScreen extends Component {
 
@@ -18,8 +19,13 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Header />
-        <Text>Home Screen</Text>
+        <Header/>
+        <View style={styles.goal_container}>
+          <Text>Component 1</Text>
+        </View>
+        <View style={styles.history_container}>
+          <Text>Component 2</Text>
+        </View>
       </View>
     )
   }
@@ -35,4 +41,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
   },
+
+  goal_container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: vw(100)- 32,
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop: 20,
+    borderWidth: 5,
+    borderColor: '#2f225b',
+    borderRadius: 20
+  },
+
+  history_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2f225b',
+    margin: 16,
+    width: vw(100) - 32,
+    borderRadius: 20
+  }
 })
