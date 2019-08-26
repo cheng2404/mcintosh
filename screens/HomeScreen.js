d7a1ca1229c6836b9264e61ab4da853aba69f729
Chengcheng Bao <c.bao@uq.net.au>
2019-08-26 23:28:19 +1000

    Saving History Title done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Hr,
 } from 'react-native';
 
 import Header from '../components/Dashboard/Header'
 import { vw } from 'react-native-expo-viewport-units';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 export default class HomeScreen extends Component {
 
@@ -24,7 +26,23 @@ export default class HomeScreen extends Component {
           <Text>Component 1</Text>
         </View>
         <View style={styles.history_container}>
-          <Text>Component 2</Text>
+          <View style={styles.history_header}>
+              <Text style={styles.headerText}>Saving History</Text>
+              <Text style={styles.tempLink}>View more</Text>
+          </View>
+          <View style = {styles.lineStyle} />
+          <View style={styles.history_section}>
+            <Text>section1</Text>
+          </View>
+          <View style={styles.history_section}>
+            <Text>section2</Text>
+          </View>
+          <View style={styles.history_section}>
+            <Text>section3</Text>
+          </View>
+          <View style={styles.history_section}>
+            <Text>section4</Text>
+          </View>
         </View>
       </View>
     )
@@ -58,11 +76,59 @@ const styles = StyleSheet.create({
 
   history_container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#2f225b',
     margin: 16,
     width: vw(100) - 32,
-    borderRadius: 20
+    borderRadius: 20,
+  },
+
+  history_header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop: 10,
+    width: vw(100) - 50,
+  },
+
+  history_section: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: vw(100)- 32,
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop: 20,
+    borderWidth: 5,
+    borderColor: '#2f225b',
+    borderRadius: 20,
+    backgroundColor: '#fff',
+  },
+
+  headerText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+
+  tempLink: {
+    fontSize: 12,
+    textDecorationLine: "underline",
+    marginLeft: 100,
+    lineHeight: 35,
+    color: "#fff",
+  },
+
+  lineStyle:{
+    backgroundColor: '#fff',
+    height: 8,
+    width: vw(100)- 50,
+    margin: 2,
   }
 })
