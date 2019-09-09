b8a8f3b1abcfdea95877773253d0c71c70e82a79
Xiaolong chen <xiaolong.chen@uq.net.au>
2019-09-10 00:29:57 +1000

    Add plan & profile screen to navigator

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -2,12 +2,16 @@ import { createStackNavigator } from 'react-navigation'
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import SetSavingScreen from '../screens/SetSavingScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import PlanScreen from '../screens/PlanScreen';
 
 const AuthNavigatour = createStackNavigator(
   {
     Login: { screen: LoginScreen },
     Register: { screen: RegisterScreen },
-    SetSaving: { screen: SetSavingScreen }
+    SetSaving: { screen: SetSavingScreen },
+    Profile: {screen: ProfileScreen},
+    Plan: {screen: PlanScreen}
   },
   {
     headerMode: 'none'
